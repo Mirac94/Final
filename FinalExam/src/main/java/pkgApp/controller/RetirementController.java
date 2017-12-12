87574821366dfb24122895c2ec9ca2f0d4b96017
Mirac <Mirac@10.0.0.16>
2017-12-12 14:11:09 -0500

    commit

--- FinalExam/src/main/java/pkgApp/controller/RetirementController.java
+++ FinalExam/src/main/java/pkgApp/controller/RetirementController.java
@@ -15,7 +15,22 @@ public class RetirementController implements Initializable {
 	private RetirementApp mainApp = null;
 	
 	@FXML
-	private TextField txtYearsToWork;
+	private TextField YearsToWork;
+	@FXML
+	private TextField AnnualReturnWork;
+	@FXML
+	private TextField YearsRetired;
+	@FXML
+	private TextField AnnualReturnRetired;
+	@FXML
+	private TextField RequiredIncome;
+	@FXML
+	private TextField MonthlySSI;
+	@FXML
+	private TextField SaveEachMonth;
+	@FXML
+	private TextField NeedToSave;
+	
 	
 
 	public RetirementApp getMainApp() {
@@ -34,14 +49,18 @@ public class RetirementController implements Initializable {
 	public void btnClear(ActionEvent event) {
 		System.out.println("Clear pressed");
 		
-		//	TODO: Clear all the text inputs
+		
+		AnnualReturnWork.clear();
+		RequiredIncome.clear();
+		YearsToWork.clear();
+		AnnualReturnRetired.clear();
+		YearsRetired.clear();
+		MonthlySSI.clear();
 	}
 	
 	@FXML
 	public void btnCalculate(ActionEvent event) {
 		
 		//	TODO: Call AmountToSave and TotalAmountSaved and populate 
-		
-	}
-	
+	}		
 }
